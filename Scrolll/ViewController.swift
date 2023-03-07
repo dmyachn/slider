95eeb99b4b9a3b27dc4bd9d7ffd4cf4fac386d4a
Dmitry <d.myachn@gmail.com>
2023-03-07 22:59:01 +0300

    Initial Commit

---
+++ Scrolll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scrolll
+//
+//  Created by Dmitiy Myachin on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
